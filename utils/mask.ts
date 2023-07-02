dce3bfaedb8630a843a2b925550c3274b61665b9
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2023-07-02 14:51:10 -0300

    Update mask.ts

--- utils/mask.ts
+++ utils/mask.ts
@@ -4,7 +4,6 @@ const Mask = {
       .replace(/\D+/g, "")
       .replace(/(\d{2})(\d)/, "($1)$2")
       .replace(/(\d{5})(\d)/, " $1-$2")
-    //   .replace(/(\d{4})(\d)/, "$1/$2")
       .replace(/(-\d{4})\d+?$/, "$1");
   },
   cnpj: (value: string) => {

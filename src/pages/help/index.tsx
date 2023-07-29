bbac67bb1b1dca7e2745f28b92bccab8d968d2c4
Luiz Felipe <68920578+luiz-k-alencar@users.noreply.github.com>
2023-07-29 15:20:43 -0300

    help page

--- src/pages/help/index.tsx
+++ src/pages/help/index.tsx
@@ -1,17 +1,58 @@
 import { FC } from "react";
 
 import Layout from "@/components/layout";
+import Form from "@/components/form";
+
+import { User } from "@/types/user";
 
 import styles from "./styles.module.scss";
+import { GetServerSideProps } from "next";
 
-const Help: FC = () => {
+interface Props {
+  user: User;
+}
+
+const Help: FC<Props> = ({user}: Props) => {
   return (
-    <div className={styles.container}>
-      <Layout header headerTitle="Suporte" sidebar active={4}>
-        <div>Suporte</div>
-      </Layout>
-    </div>
+    <Layout header headerTitle="Suporte" sidebar active={2}>
+      <div className={styles.container}>
+        <div className={styles.textContent}>
+          <h1>
+            Seu feedback é muito importante para o desenvolvimento da
+            plataforma.
+          </h1>
+          <p>
+            Se você tiver alguma dúvida ou sugestão, entre em contato conosco
+            pelo formulário ao lado.
+          </p>
+          <p>Estamos aqui para te ajudar.</p>
+        </div>
+        <Form user={user}/>
+      </div>
+    </Layout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { req } = context;
+  const { token } = req.cookies;
+
+  const userResponse = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/user/profile`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  const user = await userResponse.json();
+
+  return {
+    props: {
+      user,
+    },
+  };
 };
 
 export default Help;
